f5ad3c10e41d077a2b8be630d2a24109980255c1
Orion Rawlings <orionrawlings@gmail.com>
2024-11-16 15:17:05 -0700

    Added list of words, welcome message, and declared name of guessing fun.

--- hangman.py
+++ hangman.py
@@ -1,7 +1,10 @@
 import random
 
 def main():
-    pass
+    word_bank = ["mario", "metroid", "zelda", "nintendo", "capcom", "sony", "dk", "mariokart", "darksouls", "gaming"]
+    print("Welcome to the Video Game edition of Hangman!\nHere are some rules to be aware of:\n   1. Please only enter 1 letter at a time.\n   2. There will be no punctuation in these words.\n   3. Don't worry about being case sensitive! We'll cover that for you.\n   4. Please have fun!\nWith that out of the way, let's begin!")
+
+
 
 if __name__ == '__main__':
     main()
